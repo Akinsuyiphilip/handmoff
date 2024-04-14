0787e4bc34fe4428b5052b1fee1897e88605b0f4
Samson Okunola <smsnmicheal@gmail.com>
2024-04-14 16:44:19 +0100

    patch

--- client/src/pages/signin/index.tsx
+++ client/src/pages/signin/index.tsx
@@ -32,7 +32,7 @@ const Signin = () => {
 			} = data
 			signIn(user, token)
 			toast.success(message)
-			router.push("/rooms")
+			router.push("/room-types")
 		},
 		onError: ({ response }: HttpError) => {
 			const { message } = response.data
